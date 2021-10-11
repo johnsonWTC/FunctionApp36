6e606ac93c38c5dcaa4d435859aeca9f77c00c45
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-11 21:57:06 +0200

    Functions

---
+++ Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp36
+{
+    class Book
+    {
+        public int BookID { get; set; }
+        public string BookName { get; set; }
+    }
+}
